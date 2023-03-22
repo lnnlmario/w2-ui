c03358cb6bb62288d73df0bf51d6f21a867197cf
王伟 <wangw@vastio.com>
2023-03-22 14:57:45 +0800

    树形控件

---
+++ w2docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+const sidebar = {
+  "/": [
+    {
+      text: "快速开始",
+      link: "/",
+    },
+    {
+      text: "Form 表单组件",
+    },
+    {
+      text: "Data 数据展示",
+    },
+    {
+      text: "Navigation 导航",
+    },
+    {
+      text: "Feedback 反馈组件",
+    },
+    {
+      text: "Others 其他",
+    },
+  ],
+};
+
+const config = {
+  themeConfig: {
+    sidebar,
+  },
+};
+
+export default config;
---
+++ w2ui/tree/src/tree.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'W2Tree',
+  setup() {
+    return () => {
+      return (
+        <div>Tree</div>
+      )
+    }
+  }
+});
