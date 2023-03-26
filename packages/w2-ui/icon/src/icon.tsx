fe8d1dfb7905768f5920e4d0ec1a10da1b5a0274
王伟 <wangw@vastio.com>
2023-03-26 23:27:03 +0800

    去除多余子组件

--- packages/w2-ui/icon/src/icon.tsx
+++ packages/w2-ui/icon/src/icon.tsx
@@ -1,16 +1,33 @@
-import { defineComponent, computed } from 'vue'
-
+import { toRefs, computed, defineComponent, defineAsyncComponent } from 'vue'
 import { iconProps, IconProps } from './iconTypes'
-
-import Svg from './svg'
 
 export default defineComponent({
   name: 'W2Icon',
   props: iconProps,
   setup(props: IconProps) {
+    // 动态导入图标组件
+    const AsyncSvg = defineAsyncComponent(() => import(`./components/${props.type}`))
+
+    const { size, color } = toRefs(props)
+
+    // 设置图标颜色
+    const svgStyle = computed(() => ({
+      fill: color.value
+    }))
+
     return () => {
       return (
-        <Svg {...props} />
+        <svg
+          class="icon"
+          width={`${size.value}px`}
+          height={`${size.value}px`}
+          style={svgStyle.value}
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <AsyncSvg />
+        </svg>
       )
     }
   }
