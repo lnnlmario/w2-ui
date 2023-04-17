e93397d24fb0ed4dee7470dd616401d9996126ca
王伟 <wangw@vastio.com>
2023-04-17 21:44:55 +0800

    添加vitest测试框架

--- packages/hooks/useNamespace/index.ts
+++ packages/hooks/useNamespace/index.ts
@@ -3,15 +3,26 @@ export type IUseNamespace = {
 	e: (el: string) => string
 	m: (modifier: string) => string
 	em: (el: string, modifier: string) => string
+	is: (name: string, ...args: [boolean | undefined] | []) => string
 }
 
-const createBem = (block: string, element?: string, modifier?: string): string => {
+const defaultNamespace = 'w2'
+const statePrefix = 'is'
+
+const createBem = (block: string, blockSuffix?: string, element?: string, modifier?: string): string => {
 	let cls = block
 
+	// el-form-item__content--error
+	if (blockSuffix) {
+		cls += `-${blockSuffix}`
+	}
+
+	// el-form-item__content
 	if (element) {
 		cls += `__${element}`
 	}
 
+	// el-form-item__content--error
 	if (modifier) {
 		cls += `--${modifier}`
 	}
@@ -19,13 +30,18 @@ const createBem = (block: string, element?: string, modifier?: string): string =
 	return cls
 }
 
-export const useNamespace = (block: string, needDot = false): IUseNamespace => {
-	const ns = needDot ? `.w2-${block}` : `w2-${block}`
+export const useNamespace = (block: string, blockSuffix = ''): IUseNamespace => {
+	// const ns = needDot ? `.w2-${block}` : `w2-${block}`
+	const ns = `${defaultNamespace}-${block}`
 
-	const b = () => createBem(ns)
-	const e = (el: string) => createBem(ns, el)
-	const m = (modifier: string) => createBem(ns, '', modifier)
-	const em = (el: string, modifier: string) => createBem(ns, el, modifier)
+	const b = () => createBem(ns, blockSuffix)
+	const e = (el: string) => createBem(ns, blockSuffix, el)
+	const m = (modifier: string) => createBem(ns, blockSuffix, '', modifier)
+	const em = (el: string, modifier: string) => createBem(ns, blockSuffix, el, modifier)
+	const is = (name: string, ...args: [boolean | undefined] | []) => {
+		const state = args.length >= 1 ? args[0] : true
+		return name && state ? `${statePrefix}-${name}` : ''
+	}
 
-	return { b, e, m, em }
+	return { b, e, m, em, is }
 }
